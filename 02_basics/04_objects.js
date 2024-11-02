16e8f67d0869c2c5da0898b1cf10a51a0e697767
MANISH1204 <47654375+MANISH1204@users.noreply.github.com>
2024-11-02 12:38:29 +0000

    Jsontalks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,4 +24,20 @@ const obj1 = {1:"a", 2: "b"}
 const obj2 = {3:"a", 4: "b"}
 // const obj3 = Object.assign({}, obj1, obj2)
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
+
+
+const course = {
+    courseName : "Javascript",
+    price: 999,
+    courseInstructor: "Hitesh"
+}
+
+
+//course.courseInstructor
+const {courseInstructor: instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+
